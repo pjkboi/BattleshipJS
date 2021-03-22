1193e62b63c6115b12a5e283ca8f3ac7cf8e0811
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2021-03-22 14:18:34 -0400

    can randomly generate computer board

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 
     //Creating the board
-    function createBoard(grid, squares, width){
+    function createBoard(grid, squares){
         for(let i = 0; i<width*width; i++){
             const square = document.createElement('div');
             square.dataset.id = i;
@@ -67,8 +67,40 @@ document.addEventListener('DOMContentLoaded', () =>{
                 [0, width, width*2, width*3, width*4]
             ]
         }
-    ]
+    ];
 
     //Draw the computer ships in random locations
-    
+    function generate(ship){
+        console.log(ship);
+        let randomDirection = Math.floor(Math.random() * ship.directions.length);
+        let current = ship.directions[randomDirection];
+        if(randomDirection === 0 ){
+            direction = 1;
+        }
+        if(randomDirection === 1){
+            direction = 10;
+        }
+        let randomStart = Math.abs(Math.floor(Math.random() * computerSquares.length - ship.directions[0].length * direction));
+        console.log(computerSquares);
+        const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'));
+        const isAtRightEdge = current.some(index => (randomStart + index) % width === width-1);
+        const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0);
+
+        if(!isTaken && !isAtLeftEdge && !isAtRightEdge){
+            current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ship.name));
+
+        }
+        else{
+            generate(ship)
+        }
+    }
+
+    generate(shipArray[0]);
+    generate(shipArray[1]);
+    generate(shipArray[2]);
+    generate(shipArray[3]);
+    generate(shipArray[4]);
+
+
+
 });
