744e684ab5c3cd637dfaaeb8940f5cf23f99b1ba
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2021-03-23 17:48:13 -0400

    drag and drop fixes

--- app.js
+++ app.js
@@ -155,17 +155,21 @@ document.addEventListener('DOMContentLoaded', () =>{
 
         let lastShipIndex = parseInt(shipNameWithLastId.substr(-1));
         let shipLastId = lastShipIndex + parseInt(this.dataset.id);
-        const notAllowedHorrizontal = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 1, 11, 21, 31, 41, 51, 61, 71, 81, 91]
+        const notAllowedHorrizontal = [0,10,20,30,40,50,60,70,80,90,1,11,21,31,41,51,61,71,81,91,2,12,22,32,42,52,62,72,82,92,3,13,23,33,43,53,63,73,83,93];
+        const notAllowedVertical = [99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60];        
+        let newNotAllowedHorrizontal = notAllowedHorrizontal.splice(0, 10 * lastShipIndex);
+        let newNotAllowedVertical = notAllowedVertical.splice(0, 10 * lastShipIndex);
         selectedShipIndex = parseInt(selectedShipNameWithIndex.substr(-1));
+        
 
         shipLastId = shipLastId - selectedShipIndex;
 
-        if(isHorizontal){
+        if(isHorizontal && !newNotAllowedHorrizontal.includes(shipLastId)){
             for(let i = 0; i<draggedShipLength; i++){
                 userSquares[parseInt(this.dataset.id) - selectedShipIndex + i].classList.add('taken', shipClass);
             }
         }
-        else if(!isHorizontal){
+        else if(!isHorizontal && !newNotAllowedVertical.includes(shipLastId)){
             for(let i = 0; i<draggedShipLength; i++){
                 userSquares[parseInt(this.dataset.id) -selectedShipIndex + width*i].classList.add('taken', shipClass);
             }
