52e65194451c67011770921286b5c51791000b3a
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2021-04-01 04:45:03 -0400

    polish

--- public/app.js
+++ public/app.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     const rotateButton = document.querySelector('#rotate');
     const turnDisplay = document.querySelector('#whose-go');
     const infoDisplay = document.querySelector('#info');
+    const setupButtons = document.getElementById('setup-buttons');
     const userSquares = [];
     const computerSquares = [];
     let isHorizontal = true;
@@ -105,6 +106,7 @@ document.addEventListener('DOMContentLoaded', () =>{
             playerReady(num);
             if(ready){
                 playGameMulti(socket);
+                setupButtons.style.display = 'none';
             }
         });
 
@@ -165,7 +167,7 @@ document.addEventListener('DOMContentLoaded', () =>{
         function playerConnectedOrDisconnected(num){
             let player = `.p${parseInt(num) + 1}`;
             console.log(player);
-            document.querySelector(`${player} .connected span`).classList.toggle('green');
+            document.querySelector(`${player} .connected`).classList.toggle('active');
             if(parseInt(num) === playerNum){
                 document.querySelector(player).style.fontWeight = 'bold';
             }
@@ -180,7 +182,10 @@ document.addEventListener('DOMContentLoaded', () =>{
         generate(shipArray[3]);
         generate(shipArray[4]);
 
-        startButton.addEventListener('click', playGameSingle);
+        startButton.addEventListener('click', () =>{
+            setupButtons.style.display = 'none';
+            playGameSingle()
+        });
     }
 
     //Creating the board
@@ -285,12 +290,26 @@ document.addEventListener('DOMContentLoaded', () =>{
 
         if(isHorizontal && !newNotAllowedHorrizontal.includes(shipLastId)){
             for(let i = 0; i<draggedShipLength; i++){
-                userSquares[parseInt(this.dataset.id) - selectedShipIndex + i].classList.add('taken', shipClass);
+                let directionClass;
+                if(i === 0){
+                    directionClass = 'start';
+                }
+                if(i === draggedShipLength - 1){
+                    directionClass = 'end';
+                }
+                userSquares[parseInt(this.dataset.id) - selectedShipIndex + i].classList.add('taken', 'horizontal', directionClass, shipClass);
             }
         }
         else if(!isHorizontal && !newNotAllowedVertical.includes(shipLastId)){
             for(let i = 0; i<draggedShipLength; i++){
-                userSquares[parseInt(this.dataset.id) -selectedShipIndex + width*i].classList.add('taken', shipClass);
+                let directionClass;
+                if(i === 0){
+                    directionClass = 'start';
+                }
+                if(i === draggedShipLength - 1){
+                    directionClass = 'end';
+                }
+                userSquares[parseInt(this.dataset.id) -selectedShipIndex + width*i].classList.add('taken', 'vertical', directionClass, shipClass);
             }
         }
         else{
@@ -307,6 +326,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     //Game logic for multiplayer
     function playGameMulti(socket){
+        setupButtons.style.display = 'none';
         if(isGameOver){
             return;
         }
@@ -327,7 +347,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     function playerReady(num){
         let player = `.p${parseInt(num)+ 1}`;
-        document.querySelector(`${player} .ready span`).classList.toggle('green');
+        document.querySelector(`${player} .ready`).classList.toggle('active');
     }
 
     //Game Logic for single player
@@ -403,7 +423,8 @@ document.addEventListener('DOMContentLoaded', () =>{
             square = Math.floor(Math.random() * userSquares.length);
         }
         if(!userSquares[square].classList.contains('boom')){
-            userSquares[square].classList.add('boom');
+            const hit = userSquares[square].classList.contains('taken')
+            userSquares[square].classList.add(hit ? 'boom': 'miss');
             if(userSquares[square].classList.contains('destroyer')){
                 cpuDestroyerCount++;
             }
